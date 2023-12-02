c018831ded6a60f2fa20a4f907317495df0fe763
Joel Lamata <jlamata88@gmail.com>
2023-12-02 17:48:29 +0100

    First commit with CHatGPT guide

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
+import operator
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def get_news():
+    url = 'https://news.ycombinator.com/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    posts = soup.select('.athing')
+    metadata = soup.select('.subtext')
+    news = []
 
+    for index in range(len(posts)):
+        post = posts[index]
+        title = post.select_one('.titleline').select_one('a').get_text()
+        points = int(metadata[index].select_one('.score').get_text().split()[0])
+        comments = int(metadata[index].select('a')[-1].get_text().split()[0])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        news.append({'title': title, 'order': index, 'comments': int(comments), 'points': int(points)})
 
+    return news
 
-# Press the green button in the gutter to run the script.
+def filter_news(news, filter_func, sort_key):
+    filtered_news = filter(filter_func, news)
+    return sorted(filtered_news, key=operator.itemgetter(sort_key), reverse=True)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    news = get_news()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Filter all previous entries with more than five words in the title ordered by the number of comments first.
+    more_than_five_words = filter_news(news, lambda x: len(x['title'].split()) > 5, 'comments')
+    print("Entries with more than five words in the title ordered by comments:")
+    for entry in more_than_five_words:
+        print(entry)
+
+    # Filter all previous entries with less than or equal to five words in the title ordered by points.
+    less_than_equal_five_words = filter_news(news, lambda x: len(x['title'].split()) <= 5, 'points')
+    print("\nEntries with less than or equal to five words in the title ordered by points:")
+    for entry in less_than_equal_five_words:
+        print(entry)
